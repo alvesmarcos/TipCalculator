441ca9ed5cf3ebdba7f6a57e7385dd7496caf477
alvesmarcos <alves.henrique.marcos@gmail.com>
2019-05-10 20:20:10 -0300

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Marcos Alves on 10/05/19.
+//  Copyright © 2019 Marcos Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
