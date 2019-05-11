c36655efd504d32d62622afa833ebe305e128652
alvesmarcos <alves.henrique.marcos@gmail.com>
2019-05-11 14:59:43 -0300

    feat: add toolbar to keyboard

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        subTotalTextField.becomeFirstResponder()
+        createToolbar()
     }
-
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var subTotalTextField: UITextField!
+    
+    // MARK: - Functions
+    func createToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.sizeToFit()
+        
+        // create bar button item
+        let doneBtn = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(dismissKeyboard))
+       
+        let flexibleSpace = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: self,
+            action: nil)
+        
+        // add items to toolbar
+        toolbar.setItems([flexibleSpace, doneBtn], animated: true)
+        
+        // add toolbar to keyboard
+        subTotalTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc func dismissKeyboard() {
+        subTotalTextField.resignFirstResponder()
+    }
 }
 
